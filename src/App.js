f06ed1f8a262f46002ba1649709d3ecc3bffc3b3
ALJAZARIhadi <hadikezzhe@gmail.com>
2026-09-01 15:36:15 +0300

    Navbar for all pages before the Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from  'react-router-dom';
-import HomePage from './HomePage';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
 function App() {
   return (
     <div className="App">
-      
-
-  {/*Today I learend the <Link /> tag in react */}
-
+      {/*Today I learend the <Link /> tag in react */}
+      <div>
+        <ul>
+          <li>
+            <button>Home</button>
+          </li>
+          <li>
+            <button>Events</button>
+          </li>
+          <li>
+            <button>Contact</button>
+          </li>
+        </ul>
+      </div>
       {/*Routes the logic here is like the lojic in the contex because we import it from then used it*/}
       <Routes>
-        <Route path='/hello' element={<h1>Hello World</h1>}/>
-        <Route path='/' element={<HomePage/>}/>
+        <Route path="/hello" element={<h1>Hello World</h1>} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </div>
   );
