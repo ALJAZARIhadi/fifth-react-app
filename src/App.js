674f6a217f7d58794c420cd1443992d8c39beb3e
ABDULHADI KEZZA <hadikezzhe@gmail.com>
2026-09-01 18:32:57 +0300

    Practicing "Link" Tag in react

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import HomePage from "./HomePage";
-import Hello from './Hello'
+import Hello from "./Hello";
+import EventsList from './EventsList'
 function App() {
   return (
     <div className="App">
       {/*Today I learend the <Link /> tag in react */}
-      <div>
-        <ul>
-          <li>
-            <button>Home</button>
-          </li>
-          <li>
-            <button>Events</button>
-          </li>
-          <li>
-            <button>Contact</button>
-          </li>
-        </ul>
+      <div style={{fontSize:"30px"}}>
+        <Link to="/">
+        <button style={{fontSize:"30px"}}>Home</button>
+        </Link>
+        <Link to="/Events">
+        <button style={{fontSize:"30px"}}>Events</button>
+        </Link>
+        <Link to="hello">
+        <button style={{fontSize:"30px"}}>Contact</button>
+        </Link>
       </div>
       {/*Routes the logic here is like the lojic in the contex because we import it from then used it*/}
       <Routes>
-        <Route path="/hello" element={<Hello/>} />
+        <Route path="/hello" element={<Hello />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/Events" element={<EventsList />} />
       </Routes>
     </div>
   );
