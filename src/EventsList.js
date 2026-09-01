081c210bcfd6426ead8ceac2b2ebcc637f099063
ABDULHADI KEZZA <hadikezzhe@gmail.com>
2026-09-01 18:34:44 +0300

    Create EventsList component to display events

---
+++ src/EventsList.js
@@ -0,0 +1,16 @@
+export default function Events()
+{
+    return(
+        <>
+            <div>
+                <h1>Event Title</h1>
+                <p>sdlkfj lkdjkdjs kjf</p>
+            </div>
+            
+            <div>
+                <h1>Event Title</h1>
+                <p>sdlkfj lkdjkdjs kjf</p>
+            </div>
+        </>
+    )
+}
