ae8eff11243f269e36183695f5b8bbee54012327
ABDULHADI KEZZA <hadikezzhe@gmail.com>
2026-09-01 18:33:27 +0300

    Add Link component to Hello.js for navigation

--- src/Hello.js
+++ src/Hello.js
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Hello(){
     return(
     <>
         <h1>Hello World this is an external Component That Called by reoutes </h1>
-    </>)
+        <Link to="/">
+        <div style={{background:"green" , padding:"20px"}}>
+            <h1>Go Home Page</h1>
+        </div>
+        </Link>
+    </>
+    )
 }
