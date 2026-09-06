52e276818ce61eed4e07d78c1c049dc2faab8c62
ABDULHADI KEZZA <hadikezzhe@gmail.com>
2026-09-07 01:57:37 +0300

    Create NewEvent component for event creation

---
+++ src/NewEvent.js
@@ -0,0 +1,9 @@
+export default function NewEvent()
+{
+    return(
+        <>
+            <h1>Creating a new Event</h1>
+            <button style={{background:"blue" , color:"white", padding:"10px"}}>Create</button>
+        </>
+    )
+}
