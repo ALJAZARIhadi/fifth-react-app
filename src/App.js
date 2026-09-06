11ae19e91ffdce24241ad453b9241626b85057f4
ABDULHADI KEZZA <hadikezzhe@gmail.com>
2026-09-07 01:56:51 +0300

    Refactor App.js for improved readability and structure

--- src/App.js
+++ src/App.js
@@ -1,55 +1,60 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes, Link } from "react-router-dom";
 import HomePage from "./HomePage";
 import Hello from "./Hello";
-import EventsList from './EventsList'
+import EventsList from "./EventsList";
 import EventDetails from "./EventDetails";
-import {eventsContext} from "./contexts/eventsContext"
+import { eventsContext } from "./contexts/eventsContext";
 import NotFound from "./NotFound";
+import NewEvent from "./NewEvent";
+import EventLayout from "./EventLayout";
 function App() {
-let eventsData = [
-  {
-    id: 1,
-    title: "Hello world",
-    body: "dsj jjs dj kjs ",
-  },
-  {
-    id: 2,
-    title: "Hi world",
-    body: "dsj jjs dj kjs ",
-  },
-  {
-    id: 3,
-    title: "Welcome world",
-    body: "dsj jjs dj kjs ",
-  },
-];
+  let eventsData = [
+    {
+      id: 1,
+      title: "Hello world",
+      body: "dsj jjs dj kjs ",
+    },
+    {
+      id: 2,
+      title: "Hi world",
+      body: "dsj jjs dj kjs ",
+    },
+    {
+      id: 3,
+      title: "Welcome world",
+      body: "dsj jjs dj kjs ",
+    },
+  ];
 
   return (
-    <eventsContext.Provider value={eventsData}> 
-    <div className="App">
-      {/*Today I learend the <Link /> tag in react */}
-      <div style={{fontSize:"30px"}}>
-        <Link to="/">
-        <button style={{fontSize:"30px"}}>Home</button>
-        </Link>
-        <Link to="/Events">
-        <button style={{fontSize:"30px"}}>Events</button>
-        </Link>
-        <Link to="hello">
-        <button style={{fontSize:"30px"}}>Contact</button>
-        </Link>
+    <eventsContext.Provider value={eventsData}>
+      <div className="App">
+        {/*Today I learend the <Link /> tag in react */}
+        <div style={{ fontSize: "30px" }}>
+          <Link to="/">
+            <button style={{ fontSize: "30px" }}>Home</button>
+          </Link>
+          <Link to="/Events">
+            <button style={{ fontSize: "30px" }}>Events</button>
+          </Link>
+          <Link to="hello">
+            <button style={{ fontSize: "30px" }}>Contact</button>
+          </Link>
+        </div>
+        {/*Routes the logic here is like the lojic in the contex because we import it from then used it*/}
+        <Routes>
+          <Route path="/hello" element={<Hello />} />
+          <Route path="/events" element={<EventLayout />}>
+            <Route index element={<EventsList />} />
+            <Route path=":eventId" element={<EventDetails />} />
+            <Route path="new" element={<NewEvent />} />
+            <Route path="delete" element={<EventDetails />} />
+          </Route>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
       </div>
-      {/*Routes the logic here is like the lojic in the contex because we import it from then used it*/}
-      <Routes>
-        <Route path="/hello" element={<Hello />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/Events" element={<EventsList />} />
-        <Route path="/eventDetails/:eventId" element={<EventDetails />} />
-        <Route path="*" element={<NotFound/>}></Route>
-      </Routes>
-    </div>
     </eventsContext.Provider>
   );
 }
