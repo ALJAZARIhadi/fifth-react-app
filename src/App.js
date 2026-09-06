816814704849dd698177801110fb31d6760d2494
ABDULHADI KEZZA <hadikezzhe@gmail.com>
2026-09-06 03:37:39 +0300

    Add event details route and context provider

--- src/App.js
+++ src/App.js
@@ -4,8 +4,30 @@ import { Route, Routes, Link } from "react-router-dom";
 import HomePage from "./HomePage";
 import Hello from "./Hello";
 import EventsList from './EventsList'
+import EventDetails from "./EventDetails";
+import {eventsContext} from "./contexts/eventsContext"
+import NotFound from "./NotFound";
 function App() {
+let eventsData = [
+  {
+    id: 1,
+    title: "Hello world",
+    body: "dsj jjs dj kjs ",
+  },
+  {
+    id: 2,
+    title: "Hi world",
+    body: "dsj jjs dj kjs ",
+  },
+  {
+    id: 3,
+    title: "Welcome world",
+    body: "dsj jjs dj kjs ",
+  },
+];
+
   return (
+    <eventsContext.Provider value={eventsData}> 
     <div className="App">
       {/*Today I learend the <Link /> tag in react */}
       <div style={{fontSize:"30px"}}>
@@ -24,8 +46,11 @@ function App() {
         <Route path="/hello" element={<Hello />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/Events" element={<EventsList />} />
+        <Route path="/eventDetails/:eventId" element={<EventDetails />} />
+        <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </div>
+    </eventsContext.Provider>
   );
 }
 
